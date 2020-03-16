71bf76dd4df9736e771b4f8d24ffe3e674f0be45
御子地 <1229432979@qq.com>
2020-03-16 21:31:19 +0800

    '创建button-group'

--- src/button.vue
+++ src/button.vue
@@ -36,7 +36,7 @@ export default {
   height: var(--button-height);
   font-size: var(--font-size);
   padding: 0 1em;
-  border-radius: var(--button-radius);
+  border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
   display: inline-flex;
