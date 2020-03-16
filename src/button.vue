7095a4168cfa291411fbafe46cfa521c6211b4a6
御子地 <1229432979@qq.com>
2020-03-16 15:00:23 +0800

    '加入parcel将button组件改为单文件组件'

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='scss'>
+.g-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding: 0 1em;
+  border-radius: var(--button-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
