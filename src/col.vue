4e910c721c7a8b343d9498d61398c756252399c8
1229432979@qq.com <1229432979@qq.com>
2020-04-27 16:57:59 +0800

    添加offset和gutter

--- src/col.vue
+++ src/col.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2020-04-26 17:28:13
- * @LastEditTime: 2020-04-26 17:41:26
+ * @LastEditTime: 2020-04-27 16:49:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ftmb-clientc:\Users\Administrator\Desktop\learn\wheel_c\src\col.vue
  -->
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]" :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
@@ -15,10 +15,18 @@
 <script>
 export default {
 name: 'GuluCol',
+data() {
+  return {
+    gutter: 0
+  }
+},
 props:{
   span:{
-    type: [Number, String]
-  }
+    type: [Number, String],
+  },
+  offset:{
+    type: [Number, String],
+  },
 }
 }
 </script>
@@ -34,6 +42,12 @@ props:{
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,21 +1,31 @@
 <!--
  * @Author: your name
  * @Date: 2020-04-26 17:28:06
- * @LastEditTime: 2020-04-26 17:34:05
+ * @LastEditTime: 2020-04-27 16:55:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ftmb-clientc:\Users\Administrator\Desktop\learn\wheel_c\src\row.vue
  -->
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 +'px', marginRight: -gutter/2 +'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-name: 'GuluRow'
-}
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
+  }
+};
 </script>
 
 <style scoped>
