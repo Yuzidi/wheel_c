28b9a2bc66639b2d1ffd4dde533637f3f1c46385
1229432979@qq.com <1229432979@qq.com>
2020-05-07 16:22:13 +0800

    解决手动关闭toast后再生成toast不执行close 和 toast动画

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-05 10:49:24
- * @LastEditTime: 2020-05-07 15:57:08
+ * @LastEditTime: 2020-05-07 16:14:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \wheel_c\src\plugin.js
@@ -13,16 +13,20 @@ export default {
   install(Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
       if(currentToast) {currentToast.close()}
-      currentToast = createToast({Vue, message, propsData: toastOptions})
+      currentToast = createToast({Vue, message, propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
+        }})
     }
   }
 }
 
-function createToast({Vue, message, propsData}) {
+function createToast({Vue, message, propsData, onClose}) {
   let Constructor = Vue.extend(Toast)
   let toast = new Constructor({propsData})
   toast.$slots.default = [message]
   toast.$mount()
+  toast.$on('close', onClose)
   document.body.appendChild(toast.$el)
   return toast
 }
