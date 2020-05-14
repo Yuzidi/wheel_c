8dbd50b27e3e34b8cb2945e4c2f26fda75c1e3d9
1229432979@qq.com <1229432979@qq.com>
2020-05-14 15:22:47 +0800

    使用表驱动编程重构代码

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-05-12 14:40:23
- * @LastEditTime: 2020-05-13 18:09:07
+ * @LastEditTime: 2020-05-14 15:19:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \wheel_c\src\popover.vue
@@ -47,24 +47,29 @@ export default {
   methods: {
     positionContent() {
       const { contentWrapper, triggerWrapper } = this.$refs;
-      const { height: height2 } = contentWrapper.getBoundingClientRect();
       document.body.appendChild(contentWrapper);
+      const { height: height2 } = contentWrapper.getBoundingClientRect();
       const { top, left, height, width } = triggerWrapper.getBoundingClientRect();
-      if (this.position === "top") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + window.scrollY + "px";
-      } else if (this.position === "bottom") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + height + window.scrollY + "px";
-      } else if (this.position === "left") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
-      } else if (this.position === "right") {
-        contentWrapper.style.left = left + width + window.scrollX + "px";
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
+      let position = {
+        top: {
+          left: left + window.scrollX,
+          top: top + window.scrollY
+        },
+        bottom: {
+          left: left + window.scrollX,
+          top: top + height + window.scrollY
+        },
+        left: {
+          left: left + window.scrollX,
+          top: top + window.scrollY + (height - height2) / 2
+        },
+        right: {
+          left: left + width + window.scrollX,
+          top: top + window.scrollY + (height - height2) / 2
+        },
       }
+      contentWrapper.style.left = position[this.position].left + 'px'
+      contentWrapper.style.top = position[this.position].top + 'px'
     },
     onClickDocument(e) {
       if (
