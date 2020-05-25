88fae452a8c863aad4d018d669a0fbd16fc9deec
1229432979@qq.com <1229432979@qq.com>
2020-05-25 14:35:08 +0800

    换成vue-cli@3

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <p>11</p>
+    <g-cascader :source='source' popover-class-name='xxx'></g-cascader>
+    <p>322</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      source: [{
+        name: '浙江',
+        children: [{
+            name: '杭州',
+            children: [{
+                name: '上城区'
+              },
+              {
+                name: '下城区'
+              },
+              {
+                name: '江干区'
+              },
+            ]
+          },
+          {
+            name: '嘉兴',
+            children: [{
+                name: '南湖区'
+              },
+              {
+                name: '秀洲区'
+              },
+              {
+                name: '嘉善区'
+              },
+            ]
+          },
+          {
+            name: '啊啊'
+          },
+        ]
+      },
+      {
+        name: '江西省',
+        children: [{
+            name: '南昌',
+            children: [{
+                name: '新建县'
+              },
+              {
+                name: '经济开发区'
+              }
+            ]
+          },
+          {
+            name: '上饶',
+            children: [{
+              name: '鄱阳县'
+            }]
+          },
+        ]
+      }
+    ]
+    }
+  },
+  components: {
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,111 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Button from './button.vue'
+import Icon from './icon.vue'
+import ButtonGroup from './button-group'
+import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Sider from './sider.vue'
+import Content from './content.vue'
+import Footer from './footer.vue'
+import Toast from './toast.vue'
+import plugin from './plugin.js'
+import Tabs from './tabs.vue'
+import TabsHead from './tabs-head.vue'
+import TabsBody from './tabs-body.vue'
+import TabsItem from './tabs-item.vue'
+import TabsPane from './tabs-pane.vue'
+import Popover from './popover.vue'
+import Collapse from './collapse.vue'
+import CollapseItem from './collapse-item.vue'
+import Cascader from './cascader.vue'
+
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-sider', Sider)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-toast', Toast)
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-popover', Popover)
+Vue.component('g-collapse', Collapse)
+Vue.component('g-collapse-item', CollapseItem)
+Vue.component('g-cascader', Cascader)
+Vue.use(plugin)
+
+import './varScss.scss'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  data: {
+    source: [{
+        name: '浙江',
+        children: [{
+            name: '杭州',
+            children: [{
+                name: '上城区'
+              },
+              {
+                name: '下城区'
+              },
+              {
+                name: '江干区'
+              },
+            ]
+          },
+          {
+            name: '嘉兴',
+            children: [{
+                name: '南湖区'
+              },
+              {
+                name: '秀洲区'
+              },
+              {
+                name: '嘉善区'
+              },
+            ]
+          },
+          {
+            name: '啊啊'
+          },
+        ]
+      },
+      {
+        name: '江西省',
+        children: [{
+            name: '南昌',
+            children: [{
+                name: '新建县'
+              },
+              {
+                name: '经济开发区'
+              }
+            ]
+          },
+          {
+            name: '上饶',
+            children: [{
+              name: '鄱阳县'
+            }]
+          },
+        ]
+      }
+    ]
+  },
+  render: h => h(App)
+}).$mount("#app");
